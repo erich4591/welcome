42f6919b7f7f66905b8bee497a5f1e9bd38c4845
Eric Hall <eharper8367@gmail.com>
2017-04-20 15:23:22 -0400

    Upload

---
+++ src/welcome/welcome.java
@@ -0,0 +1,9 @@
+package welcome;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to UNC Pembroke");
+		System.out.println("Java Programming is fun");
+	}
+}
